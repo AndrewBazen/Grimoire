4331e9afb8668eac78c8b55dab477f732b4ce18c
AndrewBazen <142728970+AndrewBazen@users.noreply.github.com>
2025-05-21 00:38:54 +0200

    Add initial project structure for Grimoire app
    - Created .gitignore file to exclude unnecessary files and directories.
    - Added main application files: App.xaml, App.xaml.cs, AppShell.xaml, and AppShell.xaml.cs.
    - Established project configuration in Grimoire.csproj and solution file Grimoire.sln.
    - Implemented main page layout in MainPage.xaml and its code-behind MainPage.xaml.cs.
    - Set up MauiProgram.cs for application initialization.
    - Included platform-specific files for Android, iOS, and MacCatalyst.
    - Added resources such as app icons, fonts, images, and styles for the UI.
    - Configured launch settings for the application.
    - Ensured compatibility with multiple platforms including Android, iOS, and Windows.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Grimoire;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Grimoire;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
